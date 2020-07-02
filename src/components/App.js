a75f83a6f3309a9092454eefdd05254f562bc0b2
Felix Jr Lazarte <felix.lazarte@paymaya.com>
2020-07-02 16:30:10 +0800

    update app

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
+
+import Dashboard from 'scenes/Dashboard';
+import Transactions from 'scenes/Transactions';
+import Invoices from 'scenes/Invoices';
+import BillsPay from 'scenes/BillsPay';
+
 import Appbar from './Appbar';
 import SideBar from './Sidebar';
-import Dashboard from '../scenes/Dashboard';
-import Transactions from '../scenes/Transactions';
-import Invoices from '../scenes/Invoices';
-import BillsPay from '../scenes/BillsPay';
 
 const routes = [
   { displayText: "dashboard", value: "" },
@@ -35,22 +34,20 @@ const App = () => {
   const classes = useStyles();
 
   return (
-    <Router>
-      <div className={classes.root}>
-        <CssBaseline />
-        <Appbar />
-        <SideBar items={ routes } />
-        <main className={classes.content}>
-          <Toolbar />
-          <Switch>
-            <Route exact path="/" component={ Dashboard } />
-            <Route path="/transactions" component={ Transactions } />
-            <Route path="/invoices" component={ Invoices } />
-            <Route path="/bills-pay" component={ BillsPay } />
-          </Switch>
-        </main>
-      </div>
-    </Router>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Appbar />
+      <SideBar items={ routes } />
+      <main className={classes.content}>
+        <Toolbar />
+        <Switch>
+          <Route exact path="/" component={ Dashboard } />
+          <Route path="/transactions" component={ Transactions } />
+          <Route path="/invoices" component={ Invoices } />
+          <Route path="/bills-pay" component={ BillsPay } />
+        </Switch>
+      </main>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { I18nextProvider } from "react-i18next";
+import { BrowserRouter as Router } from "react-router-dom";
+import { I18nextProvider } from 'react-i18next';
 
 import configureStore from './store';
 import i18n from "./i18n";
@@ -14,9 +15,11 @@ const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
+    <Router>
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
